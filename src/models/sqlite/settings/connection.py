425d2c8fffd1011d37e51b64ac728fbf0f0f271e
mgckaled <marcelgckaled@gmail.com>
2024-05-18 15:49:18 -0300

    feat: create db connection handler

---
+++ src/models/sqlite/settings/connection.py
@@ -0,0 +1,13 @@
+from SQLALchemy import create_engine
+
+
+class DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "sqlite:///storage.db"
+        self.__engine = None
+
+    def connect_to_db(self) -> None:
+        self.__engine = create_engine(self.__connection_string)
+
+    def get_engine(self) -> None:
+        return self.__engine
