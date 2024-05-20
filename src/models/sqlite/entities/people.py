5ac52db7e8456e4d633266c0c2648c8e7a207a73
mgckaled <marcelgckaled@gmail.com>
2024-05-20 14:35:07 -0300

    test-fix: fix entities and skip test

--- src/models/sqlite/entities/people.py
+++ src/models/sqlite/entities/people.py
@@ -5,11 +5,11 @@ from src.models.sqlite.settings.base import Base
 
 class PeopleTable(Base):
     __tablename__ = "people"
-    id = Column(name_pos=BIGINT, primary_key=True)
-    fisrt_name = Column(name_pos=String, nullable=False)
-    last_name = Column(name_pos=String, nullable=False)
-    age = Column(name_pos=BIGINT, nullable=False)
-    pet_id = Column(name_pos=BIGINT, type_pos=ForeignKey(
+    id = Column(BIGINT, primary_key=True)
+    fisrt_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    age = Column(BIGINT, nullable=False)
+    pet_id = Column(BIGINT, type_pos=ForeignKey(
         "pets.id"), nullable=False)
 
     def __repr__(self) -> str:
--- src/models/sqlite/entities/pets.py
+++ src/models/sqlite/entities/pets.py
@@ -5,9 +5,9 @@ from src.models.sqlite.settings.base import Base
 
 class PetsTable(Base):
     __tablename__ = "pets"
-    id = Column(name_pos=BIGINT, primary_key=True)
-    name = Column(name_pos=String, nullable=False)
-    type = Column(name_pos=String, nullable=False)
+    id = Column(BIGINT, primary_key=True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
 
     def __repr__(self) -> str:
         return f"Pets(id={self.id}, name={self.name!r}, type={self.type!r})"
